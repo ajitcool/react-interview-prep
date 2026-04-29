fad52dc11a9272ccb645fc26c7312ed242d9a299
ajitcool <ajitk6168@gmail.com>
2026-04-29 20:30:45 +0530

    Enhance substring generation with unique and length filters
    Added functionality to generate unique substrings and substrings of a specific length.

--- coding-challenges/sub-str.js
+++ coding-challenges/sub-str.js
@@ -6,3 +6,26 @@ for( let i =0;i<str.length;i++){
         console.log(str.slice(i,j));
     }
 }
+
+----------------------------------------------------------------------------------------
+Sirf unique substrings chahiye?
+    
+const str = "Ajit_Kumar_Singh";
+const substrings = new Set();
+for (let i = 0; i < str.length; i++) {
+  for (let j = i + 1; j <= str.length; j++) {
+    substrings.add(str.slice(i, j));
+  }
+}
+console.log([...substrings]);
+
+----------------------------------------------------------------------------------------
+
+Sirf specific length ke substrings?
+
+const str = "Ajit_Kumar_Singh";    
+const k = 3; // length 3 ke substrings
+for (let i = 0; i <= str.length - k; i++) {
+  console.log(str.slice(i, i + k));
+}
+// "Aji", "jit", "it_", "t_K" ...
