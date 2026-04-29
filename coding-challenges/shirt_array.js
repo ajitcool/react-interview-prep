419bf2074319bbef4232ae782322c9c6aad9761b
ajitcool <ajitk6168@gmail.com>
2026-04-29 22:45:16 +0530

    Log output and enhance comments in shirt_array.js
    Add output logging and comments for array operations.

--- coding-challenges/shirt_array.js
+++ coding-challenges/shirt_array.js
@@ -6,7 +6,7 @@ console.log(input.unshift(input.pop()));
 console.log(input);
 
 ///2nd coding question
-
+-----------------------------------------------------------------
 const input  =  [1,2,3,4,5,6];
 //output  = [24,20,16,12,8,4];
 
@@ -15,3 +15,10 @@ const output = input.map((num)=>{
 })
 .sort((a,b)=>b-a)
 console.log(output);
+
+-----------------------------------------------------------------
+
+    pop()- last se element ke value nikalna
+    push()- last se value ko insert karna and length return karna
+    shift()- starting se value fetch karna
+    unshift() - starting se value ko insert karna and array ke length return karna
